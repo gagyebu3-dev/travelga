31ddbbc28c6ab2188a7ada81a4b62467c407df25
gagyebu3-dev <cpa275th@gmail.com>
2026-09-19 19:24:21 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// 캐시 버전을 올리면(v1 -> v2) 이전 캐시는 자동 삭제됩니다.
+const CACHE_NAME = "salrim-ledger-v2";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192-v2.png",
+  "./icon-512-v2.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((response) => {
+          if (event.request.method === "GET" && response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
